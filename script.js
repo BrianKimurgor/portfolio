562084e324a87ce670ba42dee48a8053495ba57b
BrianKimurgor <kimurgorbrian20@gmail.com>
2023-05-15 21:45:20 +0300

    files commit

---
+++ script.js
@@ -0,0 +1,63 @@
+// Sticky Navigation Menu JS Code
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+console.log(scrollBtn);
+let val;
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+}
+//blog
+// Get the blog section element
+const blogSection = document.querySelector('.blog-section');
+
+// Get all the blog post links
+const blogPostLinks = blogSection.querySelectorAll('a');
+
+// Add a click event listener to each blog post link
+blogPostLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    // Prevent the default link behavior
+    event.preventDefault();
+
+    // Get the text of the clicked blog post link
+    const blogPostTitle = link.textContent;
+
+    // Display an alert with the blog post title
+    alert(`You clicked on: ${blogPostTitle}`);
+  });
+});
+
+// Side NavIgation Menu JS Code
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = function(){
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+}
+cancelBtn.onclick = function(){
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+}
+// Side Navigation Bar Close While We Click On Navigation Links
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click" , function() {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+  });
+}
